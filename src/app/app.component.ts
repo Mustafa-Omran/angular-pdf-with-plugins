2fda02988a02e584a536a20ad29fb42b7d270c6a
Mustafa Omran <mustafa.anwer@e-amanah.com>
2020-06-29 03:13:30 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'js-pdf-with-plugins';
+
+  readonly imageWidth = 208;
+
+  constructor(@Inject(DOCUMENT) private document) { }
+
+  ngOnInit() {
+  }
+
+  /**
+   * capture html as pdf
+   * 
+   * 
+   */
+  captureScreen() {
+    const data = this.document.getElementById('content');
+    html2canvas(data).then(canvas => {
+      const imgWidth = this.imageWidth;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jspdf('p', 'mm', 'a4');
+      const position = 0;
+
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('capture.pdf');
+    });
+  }
 }
